9ffc9ce484613e303ecefdb02e4bd63e5c235b41
Björn Schrader <BjoernSch@users.noreply.github.com>
2018-02-06 14:19:57 +0100

    Install from productive repository

--- stage3/02-install-package-from-repo/01-run.sh
+++ stage3/02-install-package-from-repo/01-run.sh
@@ -2,7 +2,7 @@
 
 on_chroot << EOF
 wget -O - https://packages.wlanthermo.net/wlanthermo.gpg.key | sudo apt-key add -
-wget -O /etc/apt/sources.list.d/wlanthermo-stretch-dev.list https://packages.wlanthermo.net/wlanthermo-stretch-dev.list
+wget -O /etc/apt/sources.list.d/wlanthermo-stretch.list https://packages.wlanthermo.net/wlanthermo-stretch.list
 apt update
 apt -y install wlanthermo
 EOF
