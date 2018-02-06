5a9f62f96b5a6925cb4f8d0ecfb22d2da4a78628
Björn Schrader <mail@bjoern-schrader.de>
2018-02-06 14:24:17 +0100

    Fix replace command

--- stage3/03-install-setupscript/01-run.sh
+++ stage3/03-install-setupscript/01-run.sh
@@ -5,7 +5,7 @@ install -m 644 files/setup.txt             ${ROOTFS_DIR}/boot/
 
 on_chroot << EOF
 WLANThermoVersion=$(grep -e '^$_SESSION\["webGUIversion"\]' /var/www/header.php | cut -d'"' -f4)
-sed -i -e "s/XXX_VERSION_XXX/$WLANThermoVersion" /boot/setup.txt
-sed -i -e "s/XXX_DATE_XXX/$(date +%Y-%m-%d)" /boot/setup.txt
+sed -i -e "s/XXX_VERSION_XXX/${WLANThermoVersion}/" /boot/setup.txt
+sed -i -e "s/XXX_DATE_XXX/$(date +%Y-%m-%d)/" /boot/setup.txt
 echo "@reboot         root    /bin/bash -x /root/resize.sh 2>&1 > /boot/setup.log" >> /etc/crontab
 EOF
