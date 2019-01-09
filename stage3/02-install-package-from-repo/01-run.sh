142c3f7448570240d75358b3664110b19975c8f8
Björn Schrader <BjoernSch@users.noreply.github.com>
2019-01-09 14:16:20 +0100

    Remove package

--- stage3/02-install-package-from-repo/01-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-on_chroot << EOF
-wget -O - https://packages.wlanthermo.com/wlanthermo.gpg.key | sudo apt-key add -
-wget -O /etc/apt/sources.list.d/wlanthermo-stretch.list https://packages.wlanthermo.com/wlanthermo-stretch-dev.list
-apt update
-apt -y install wlanthermo
-EOF
