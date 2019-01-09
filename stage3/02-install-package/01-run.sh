4cb0564fb129360cbcc831292edf84cb952e59c1
Björn Schrader <BjoernSch@users.noreply.github.com>
2019-01-09 14:11:37 +0100

    Install package from local file

---
+++ stage3/02-install-package/01-run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+install -m 644 files/wlanthermo-*.deb             ${ROOTFS_DIR}/tmp/
+
+on_chroot << EOF
+
+wget -O - https://packages.wlanthermo.com/wlanthermo.gpg.key | sudo apt-key add -
+wget -O /etc/apt/sources.list.d/wlanthermo-stretch.list https://packages.wlanthermo.com/wlanthermo-stretch-dev.list
+
+apt update
+
+dpkg -i /tmp/wlanthermo-*.deb
+rm -f /tmp/wlanthermo-*.deb
+
+apt -yf install
+
+EOF
